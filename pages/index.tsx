8b450f087e2a6aac3c651001a90226df0cfdfc46
Gonetil <gonetil@gmail.com>
2022-08-13 20:17:16 -0300

    remove nested css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import {PostCard, Categories, PostWidget} from '../components'
 
 const posts = [
   { title: 'Just testing', excerpt : 'This is the excerpt'},
@@ -15,15 +16,13 @@ const Home: NextPage = () => {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-        {posts.map((post,index)=>( 
-          <div>
-            {post.title}
-            {post.excerpt}
-          </div>
-        ))}
+        {posts.map((post,index)=> <PostCard post={post} key={post.title} /> )}
         </div>
         <div className="lg:col-span-4 col-span-1">
-          <div className='lg:sticky relative top-8'></div>
+          <div className='lg:sticky relative top-8'>
+           <PostWidget />
+            <Categories />
+          </div>
         </div>
 
       </div>
